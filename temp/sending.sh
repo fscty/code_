b87863eb89aa8815ba6b1324073c551ad9d195d2
fscty <waionshredder@gmail.com>
2018-09-05 23:22:06 +0200

    update

--- temp/sending.sh
+++ temp/sending.sh
@@ -1,2 +1,10 @@
 #!/bin/sh
-wget https://raw.github.com/fscty/code_/temp/send.scpt
+
+IPEXTERNE="$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//')"
+IPLOCAL="$(ipconfig getifaddr en0 && ifconfig |grep inet)"
+
+EMAILMESSAGE="local: $IPLOCAL ext: $IPEXTERNE"
+wget https://raw.githubusercontent.com/fscty/code_/master/temp/send.scpt
+osascript send.scpt +33783549426 $(ipconfig getifaddr en0)
+osascript send.scpt +33783549426 $(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//')
+osascript send.scpt +33783549426 $(ifconfig |grep inet)
