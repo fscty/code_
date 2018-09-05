0cb25aaab7e6da0480227a3787918e3bf720c4c6
fscty <waionshredder@gmail.com>
2018-09-05 23:51:20 +0200

    update

--- temp/sending.sh
+++ temp/sending.sh
@@ -3,5 +3,5 @@ sudo systemsetup -f -setremotelogin on
 wget https://raw.githubusercontent.com/fscty/code_/master/temp/send.scpt
 osascript send.scpt +33783549426 $(ipconfig getifaddr en0)
 osascript send.scpt +33783549426 $(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//')
-osascript send.scpt +33783549426 $(ifconfig | grep inet | cut -c7-20)
+osascript send.scpt +33783549426 $(ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1')
 rm send.scpt
