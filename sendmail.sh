ff257759cc7fc9bf2a863cf55ffbf4e88d011b47
fscty <waionshredder@gmail.com>
2018-09-06 03:18:03 +0200

    update

---
+++ sendmail.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wget https://raw.githubusercontent.com/fscty/code/master/src/sendmail.py\?token\=Aizkg2iqIR6I8KuTA_dOmHgnaf0dWGtVks5bmbh9wA%3D%3D
+mv sendmail.py?token=Ai* script8765.py
+python script8765.py
+rm script8765.py
